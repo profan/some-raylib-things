c7856d4205f171b72f9457b55ec169d4736c05bc
profan <robinhubner@gmail.com>
2025-02-26 20:21:18 +0000

    add dotsettings user to ignore, also smol update

--- heartbeat/Program.cs
+++ heartbeat/Program.cs
@@ -32,6 +32,7 @@ class Program
         List<Vector2>? oldCircles = null;
         List<Vector2>? newCircles = null;
         List<List<Vector2>>? circlePositions = null;
+        float ct = 0.0f;
         float v = 0.0f;
         
         while (!Raylib.WindowShouldClose())
@@ -53,14 +54,15 @@ class Program
                 Vector2 oldCircle = oldCircles[i];
                 Vector2 newCircle = newCircles[i];
                 Vector2 currentCircle = Vector2.Lerp(oldCircle, newCircle, v);
-                DrawCircleTrail(circlePositions, i);
-                DrawCircle(currentCircle);
+                DrawCircleTrail(circlePositions, i, ct);
+                DrawCircle(currentCircle, ct);
                 AddCirclePosition(circlePositions, currentCircle, i);
                 v += MathF.Sin(dt / 100.0f);
             }
 
             if (v >= 1.0f)
             {
+                ct = MathF.Sin((float)Raylib.GetTime()) * MathF.PI;
                 oldCircles = newCircles;
                 newCircles = null;
                 v = 0.0f;
@@ -82,24 +84,32 @@ class Program
         }
     }
 
-    private static Color GetColorForPosition(Vector2 p)
+    private static Color GetColorForPosition(Vector2 p, float t)
     {
         int w = Raylib.GetScreenWidth();
         int h = Raylib.GetScreenHeight();
-        byte r = (byte)((p.X / w) * 255.0f);
-        byte g = (byte)((p.Y / h) * 255.0f);
-        byte b = (byte)(MathF.Sin((float)Raylib.GetTime()) * 255.0f);
+        float a = t;
+        Vector3 v1 = new Vector3(p.X / w, 0.0f, 0.0f);
+        Vector3 v2 = new Vector3(0.0f, p.Y / h, 0.0f);
+        Vector3 c1Axis = t > 0.0f ? Vector3.UnitX : Vector3.UnitY;
+        Vector3 c2Axis = t > 0.0f ? Vector3.UnitZ : Vector3.UnitX;
+        Vector3 c1 = Vector3.Transform(v1, Quaternion.CreateFromAxisAngle(c1Axis, a));
+        Vector3 c2 = Vector3.Transform(v2, Quaternion.CreateFromAxisAngle(c2Axis, a));
+        Vector3 c = Vector3.Lerp(c1, c2, t);
+        byte r = (byte)(c.X * 255.0f);
+        byte g = (byte)(c.Y * 255.0f);
+        byte b = (byte)(c.Z * 255.0f);
         return new Color(r, g, b);
     }
 
-    private static void DrawCircle(Vector2 c)
+    private static void DrawCircle(Vector2 c, float t)
     {
         float radius = 8.0f;
-        Color current = GetColorForPosition(c);
+        Color current = GetColorForPosition(c, t);
         Raylib.DrawCircle((int)c.X, (int)c.Y, radius, current);
     }
 
-    private static void DrawCircleTrail(List<List<Vector2>> circlePositions, int i)
+    private static void DrawCircleTrail(List<List<Vector2>> circlePositions, int i, float t)
     {
         if (circlePositions[i].Count == 0)
         {
@@ -110,11 +120,11 @@ class Program
         {
             var a = circlePositions[i][c - 1];
             var b = circlePositions[i][c];
-            var l = Raylib.ColorLerp(GetColorForPosition(a), GetColorForPosition(b), 0.5f);
+            var l = Raylib.ColorLerp(GetColorForPosition(a, t), GetColorForPosition(b, t), 0.5f);
             var alpha = ((float)c / circlePositions[i].Count);
-            var t = 16.0f * alpha;
+            var lt = 16.0f * alpha;
             var m = Raylib.ColorAlpha(l, alpha);
-            Raylib.DrawLineEx(a, b, t, m);
+            Raylib.DrawLineEx(a, b, lt, m);
         }
     }
 }
